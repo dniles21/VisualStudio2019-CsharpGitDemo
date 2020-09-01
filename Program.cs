8055a9c6ca3fa83555863c5cf7ae7a15074ace95
dniles777 <57576689+dniles777@users.noreply.github.com>
2020-09-01 15:19:53 -0400

    Commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace VisualStudio2019_CsharpGitDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Here is a change");
         }
     }
 }
