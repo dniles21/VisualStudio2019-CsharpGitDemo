771e219beabc76626d4dce997c9bceaee4507216
dniles777 <57576689+dniles777@users.noreply.github.com>
2020-09-01 13:42:30 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudio2019_CsharpGitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
